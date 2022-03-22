4884506e5ba791e4591e92256f620c096c604f61
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-03-22 15:33:24 +0100

    Fragment added

--- app/src/main/java/irawan/electroshock/beritaku/MainActivity.kt
+++ app/src/main/java/irawan/electroshock/beritaku/MainActivity.kt
@@ -2,10 +2,19 @@ package irawan.electroshock.beritaku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import irawan.electroshock.beritaku.databinding.ActivityMainBinding
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvNews.setupWithNavController(navController)
     }
 }
