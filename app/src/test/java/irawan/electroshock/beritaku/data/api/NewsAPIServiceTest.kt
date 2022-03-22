aeadee1782d1abd680294696cf008e81b957d38b
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-03-22 10:34:55 +0100

    Update NewsServiceAPITest

---
+++ app/src/test/java/irawan/electroshock/beritaku/data/api/NewsAPIServiceTest.kt
@@ -0,0 +1,55 @@
+package irawan.electroshock.beritaku.data.api
+
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okio.buffer
+import okio.source
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class NewsAPIServiceTest {
+
+    private lateinit var service: NewsAPIService
+    private lateinit var server:MockWebServer
+
+    @Before
+    fun setUp() {
+        server = MockWebServer()
+        service = Retrofit.Builder()
+            .baseUrl(server.url(""))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsAPIService::class.java)
+    }
+
+    private fun enqueueMockResponse(filename: String){
+        val inputStream = javaClass.classLoader!!.getResourceAsStream(filename)
+        val source = inputStream.source().buffer()
+        val mockResponse = MockResponse()
+        mockResponse.setBody(source.readString(Charsets.UTF_8))
+        server.enqueue(mockResponse)
+    }
+
+    @Test
+    fun getTheTopHeadlines_sentRequest_receivedExpected(){
+        runBlocking {
+            enqueueMockResponse("newsresponse.json")
+            val responseBody = service.getTheTopHeadlines("us",1).body()
+            val request = server.takeRequest()
+            assertThat(responseBody).isNotNull()
+            assertThat(request.path).isEqualTo("/v2/top-headlines?country=us&page=1&apiKey=82e907de3c9d4a7aa2aeeb5c678f24b3")
+        }
+    }
+
+    @After
+    fun tearDown() {
+        server.shutdown()
+    }
+}
