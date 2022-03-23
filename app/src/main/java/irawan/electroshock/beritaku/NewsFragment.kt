7fd6ea6983b731707659f7664dc59d9c31185f90
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-03-23 09:37:19 +0100

    Pagination added

--- app/src/main/java/irawan/electroshock/beritaku/NewsFragment.kt
+++ app/src/main/java/irawan/electroshock/beritaku/NewsFragment.kt
@@ -1,12 +1,15 @@
 package irawan.electroshock.beritaku
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import irawan.electroshock.beritaku.data.util.Resource
 import irawan.electroshock.beritaku.databinding.FragmentNewsBinding
 import irawan.electroshock.beritaku.presentation.adapter.NewsAdapter
@@ -21,11 +24,16 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class NewsFragment : Fragment() {
+    private val TAG = "MYTAG"
     private lateinit var viewModel: NewsViewModel
     private lateinit var newsAdapter: NewsAdapter
     private lateinit var fragmentNewsBinding: FragmentNewsBinding
     private var country = "us"
     private var page = 1
+    private var isScrolling = false
+    private var isLoading = false
+    private var isLastPage = false
+    private var pages = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -50,7 +58,14 @@ class NewsFragment : Fragment() {
                 is Resource.Success -> {
                     hideProgressBar()
                     response.data?.let {
+                        Log.i(TAG, "data ${it.articles.toList().size}")
                         newsAdapter.differ.submitList(it.articles.toList())
+                        if(it.totalResults%20 == 0) {
+                            pages = it.totalResults / 20
+                        } else {
+                            pages = it.totalResults/20+1
+                        }
+                        isLastPage = page == pages
                     }
                 }
                 is Resource.Loading -> {
@@ -71,14 +86,44 @@ class NewsFragment : Fragment() {
         fragmentNewsBinding.rvNews.apply {
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(activity)
+            addOnScrollListener(this@NewsFragment.onScrollListener)
+
         }
     }
 
     private fun showProgressBar(){
+        isLoading = true
         fragmentNewsBinding.progressBar.visibility = View.VISIBLE
     }
     private fun hideProgressBar(){
+         isLoading = false
         fragmentNewsBinding.progressBar.visibility = View.INVISIBLE
+    }
+
+    private val onScrollListener = object : RecyclerView.OnScrollListener(){
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+            if(newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                isScrolling = true
+            }
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            val layoutManager = fragmentNewsBinding.rvNews.layoutManager as LinearLayoutManager
+            val sizeOfTheCurrentList = layoutManager.itemCount
+            val visibleItems = layoutManager.childCount
+            val topPosition = layoutManager.findFirstVisibleItemPosition()
+
+            val hasReachedToEnd = topPosition+visibleItems >= sizeOfTheCurrentList
+            val shouldPaginate = !isLoading && !isLastPage && hasReachedToEnd && isScrolling
+            if(shouldPaginate){
+                page++
+                viewModel.getNewsHeadlines(country, page)
+                isScrolling = false
+            }
+
+        }
     }
 
     companion object {
