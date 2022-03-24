3cbe72021fa2ef17134bcbaa16bd0a8f3f15c79a
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-03-24 11:45:37 +0100

    Update libraries implementation gradle

--- app/src/main/java/irawan/electroshock/beritaku/presentation/viewmodel/NewsViewModelFactory.kt
+++ app/src/main/java/irawan/electroshock/beritaku/presentation/viewmodel/NewsViewModelFactory.kt
@@ -11,7 +11,8 @@ class NewsViewModelFactory(
     private val getNewsHeadlinesUseCase : GetNewsHeadlinesUseCase,
     private val getSearchedNewsUseCase : GetSearchedNewsUseCase
 ) :ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return NewsViewModel(app, getNewsHeadlinesUseCase, getSearchedNewsUseCase) as T
     }
 }
